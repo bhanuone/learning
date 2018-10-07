7e58ae0526002d3e85f541bfca6a9b1f7daa44d8
bhanu <bhanu.dev0@gmail.com>
2018-10-07 15:52:50 +0530

    b search

---
+++ algorithms/binary_search.py
@@ -0,0 +1,16 @@
+def binary_search(array, el):
+  lo = 0
+  hi = len(array) - 1    
+  while lo <= hi:
+    mid = lo + (hi - lo) / 2        
+    if el == array[mid]:
+      return mid
+    elif el > array[mid]:
+      lo = mid + 1
+    else:
+      hi = mid - 1
+    mid = (hi + lo)/2        
+  return -1
+
+print bsearch([0, 5, 13, 19, 22, 41, 55, 68, 72, 81, 98], 55)
+            
